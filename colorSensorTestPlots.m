7c48db7b1dabbb88ec96815d6d5561651f44b616
tamran <tamramn@gmail.com>
2017-02-10 19:54:16 -0500

    updated color testing code

--- colorSensorTestPlots.m
+++ colorSensorTestPlots.m
@@ -6,17 +6,24 @@ tickSize = 12;
 titleSize = 16;
 markerSize = 4;
 legSize = 16;
-lw = 1.2;
-dg = [0 0.5 0];
+lw = .9;
+dg = [0 0.7 0];
 
-%% load data from file and plot
-% myFile = fopen('fakeData.txt');
-[colorTemp, lux, R, G, B, c] = textread('fakeData.txt','%f %f %f %f %f %f');
-clf; figure (1)
+%% load calibration data from file and plot
+[colorTemp, lux, R, G, B, c] = textread('colorData.txt','Color Temp: %f K - Lux: %f - R: %f G: %f B: %f C: %f');
+n = 1:length(colorTemp); % samples
+% formatSpec = '%f %f %f %f %f %f';
+% A = fscanf(myFile,'%f %f %f %f %f %f'); % temp in K
+figure
 plot(lux,'m','Linewidth',lw);
 hold on
 plot(R,'r-','Linewidth',lw);
-plot(G,'color',[0 0.7 0],'Linewidth',lw);
+plot(G,'color',dg,'Linewidth',lw);
 plot(B,'b-','Linewidth',lw);
-plot(c,'c','Linewidth',lw);
+plot(c,'c-','Linewidth',lw);
+hold off
+xlabel('Time')
+ylabel('Channel Readings')
+
+
 
--- tcs34725/tcs34725.ino
+++ tcs34725/tcs34725.ino
@@ -1,8 +1,5 @@
 #include <Wire.h>
-#include <SPI.h>
-#include <SD.h>
 #include "Adafruit_TCS34725.h"
-
 
 /* Example code for the Adafruit TCS34725 breakout library */
 
@@ -17,11 +14,37 @@
 /* Initialise with specific int time and gain values */
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
 
-File myFile;
-int numberPoints = 1000;
+
+void getColorReading(){
+  uint16_t r, g, b, c, colorTemp, lux;
+  
+  tcs.getRawData(&r, &g, &b, &c);
+  colorTemp = tcs.calculateColorTemperature(r, g, b);
+  lux = tcs.calculateLux(r, g, b);
+
+  for (int i = 0; i<100; i++){
+    uint16_t r, g, b, c, colorTemp, lux;
+  
+    tcs.getRawData(&r, &g, &b, &c);
+    colorTemp = tcs.calculateColorTemperature(r, g, b);
+    lux = tcs.calculateLux(r, g, b);
+  
+    Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
+    Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
+    Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
+    Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
+    Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
+    Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
+    Serial.println(" ");
+  
+  }
+  
+}
+
+
 
 void setup(void) {
-  Serial.begin(9600);
+  Serial.begin(115200);
   
   if (tcs.begin()) {
     Serial.println("Found sensor");
@@ -30,40 +53,18 @@ void setup(void) {
     while (1);
   }
 
-  // open text file to save data to
-  myFile = SD.open("colorSensorData.txt", FILE_WRITE);
-
-  if (!myFile) {  // if the file didn't open properly
-    Serial.println("error opening text file");
-    return;
-  } 
-
- for (int i = 0; i<1000; i++){
-    uint16_t r, g, b, c, colorTemp, lux;
+  getColorReading();
   
-    tcs.getRawData(&r, &g, &b, &c);
-    colorTemp = tcs.calculateColorTemperature(r, g, b);
-    lux = tcs.calculateLux(r, g, b);
-  
-    myFile.print(colorTemp, DEC); Serial.print(" ");
-    myFile.print(lux, DEC); Serial.print(" ");
-    myFile.print(r, DEC); Serial.print(" ");
-    myFile.print(g, DEC); Serial.print(" ");
-    myFile.print(b, DEC); Serial.print(" ");
-    myFile.print(c, DEC); Serial.print(" ");
-    myFile.println(" ");
-  
-  }
-
 }
 
 void loop(void) {
-  
+  /*
   uint16_t r, g, b, c, colorTemp, lux;
   
   tcs.getRawData(&r, &g, &b, &c);
   colorTemp = tcs.calculateColorTemperature(r, g, b);
   lux = tcs.calculateLux(r, g, b);
+
   
   Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
   Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
@@ -72,6 +73,6 @@ void loop(void) {
   Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
   Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
   Serial.println(" ");
+  */
 
-  
 }

ca18a35accb9979d62ee57706cf8bf5c1e8b2305
Dhazineh <dsh32@duke.edu>
2017-02-22 19:55:48 -0500

    Add files via upload
    Template which allows connection to host and transmission of color sensing data acquired by sensor system

---
+++ WifiCommColorSensingTemplate.ino
@@ -0,0 +1,93 @@
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+
+const char* ssid     = "TeamHydraII";
+const char* password = "ecedesign449";
+const char* host = "10.236.113.13";
+const char* httpPort = "3000";
+
+/* Initialise with default values (int time = 2.4ms, gain = 1x) */
+/* Adafruit_TCS34725 tcs = Adafruit_TCS34725();
+/* Initialise with specific int time and gain values */
+  Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
+
+void setup(void) {
+  Serial.begin(115200);
+  delay(100);
+  if (tcs.begin()) {
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1);
+  }
+    
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+}
+
+
+int value = 0;
+void loop(void) {
+  delay(5000);
+  ++value;
+   //Connect to Host
+  Serial.print("connecting to ");
+  Serial.println(host);
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 3000;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // We now create a URI for the request
+  String url = "/";
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  // This will send the request to the server
+  uint16_t r, g, b, c, colorTemp, lux;  
+  tcs.getRawData(&r, &g, &b, &c);
+  colorTemp = tcs.calculateColorTemperature(r, g, b);
+  lux = tcs.calculateLux(r, g, b);
+  String R = "R";
+  String B = "B";
+  String G = "G";
+  String C = "C";
+  String CT = "ColorTemp";
+  String Lux= "lux";
+  
+  String PostData = R +"="+String(r) +"&"+ B+"="+String(b) +"&"+ G+"="+String(g) +"&"+ C+"="+String(c) +"&"+ CT+"="+String(colorTemp) +"&"+ Lux+"="+String(lux);
+  client.println("POST / HTTP/1.1");
+  client.println("Host: "+String(host)+":"+String(httpPort));
+  client.println("Cache-Control: no-cache");
+  client.println("Content-Type: application/x-www-form-urlencoded");
+  client.print("Content-Length: ");
+  client.println(PostData.length());
+  client.println();
+  client.println(PostData);
+  delay(500);
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  Serial.println();
+  Serial.println("closing connection");
+}

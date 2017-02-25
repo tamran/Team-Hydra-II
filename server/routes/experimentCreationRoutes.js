7765f64a3d3cf7a1431c4016b9bcdae5ec9746bb
Matthew Faw <matthewfaw@gmail.com>
2017-02-24 19:27:15 -0500

    Changes the newExperiment response data to be a json object so it's easier to parse

--- server/routes/experimentCreationRoutes.js
+++ server/routes/experimentCreationRoutes.js
@@ -1,26 +1,35 @@
-
-let experimentName = undefined;
+let experimentData = {};
 
 let setCurrentExperiment = (body) => {
-    let turbidity = body['Turbidity'].text;
-    let conc = body['Concentration'].text;
-    let numExperiments = body['Number of Measurements'].text;
-    experimentName = turbidity + conc + numExperiments;
-    console.log(experimentName);
+    const turbidity = body['Turbidity'].text;
+    const conc = body['Concentration'].text;
+    const numExperiments = body['Number of Measurements'].text;
+    const currentTime = new Date().toISOString()
+    const experimentName = turbidity + conc + numExperiments + currentTime;
+
+    experimentData = {
+        turbidity: turbidity,
+        concentration: conc,
+        numExperiments: numExperiments,
+        time: currentTime,
+        experimentName: experimentName,
+    }
+
+    console.log(experimentData);
 } 
-const getExperimentName = () => {
-    return experimentName;
+const getExperimentData = () => {
+    return experimentData;
 }
-let clearExperimentName = () => {
-    const newExperiment = experimentName;
-    experimentName = undefined;
+let clearExperimentData = () => {
+    const newExperiment = experimentData;
+    experimentData = {};
     return newExperiment;
 }
 
 let experimentCreation = (app) => {
     app.route('/newExperiment')
         .get((req,res) => {
-            res.send(getExperimentName());
+            res.send(getExperimentData());
         })
         .post((req,res) => {
             console.log(req.body);
@@ -29,7 +38,7 @@ let experimentCreation = (app) => {
         })
     app.route('/clearExperiment')
         .get((req,res)=> {
-            res.send(clearExperimentName());
+            res.send(clearExperimentData());
         })
 }
 

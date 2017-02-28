77349363acbe84fe2dbaaace3af084c39adacc9d
Matthew Faw <matthewfaw@gmail.com>
2017-02-28 10:46:44 -0500

    small fix to form format

--- src/components/TextInputField.js
+++ src/components/TextInputField.js
@@ -4,10 +4,10 @@ import { Row, Col, ControlLabel, FormControl } from 'react-bootstrap';
 const TextInputField = ({ label, fieldText, onChange }) => {
     return (
         <div>
-            <Col xs={2}>
+            <Col xs={3}>
                 <ControlLabel>{label}</ControlLabel>
             </Col>
-            <Col xs={9} xsOffset={1}>
+            <Col xs={8} xsOffset={1}>
                 <FormControl type="text" value={fieldText} onChange={(e) => onChange(label,e.target.value)} />
             </Col>
         </div>

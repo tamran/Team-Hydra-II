6a18dc9cca508d5602cf6308fae739c074888fd1
Matthew Faw <matthewfaw@gmail.com>
2017-03-06 15:35:53 -0500

    Adds the ability to post the measurements as they are taken, and if WiFi goes away, the experiments will be added to a buffer, and the data will be sent when the WiFi becomes available again

--- featherWifiCode/DataCollection.ino
+++ featherWifiCode/DataCollection.ino
@@ -1,36 +1,60 @@
 #include <aJSON.h>
 
 /**
- * Creates the Trial requested
- */
-void createTrial(String trial) {
-  post("/api/trial/" + trial, aJson.createObject());
+   Creates the Trial requested
+*/
+int createTrial(String trial) {
+  return post("/api/trial/" + trial, aJson.createObject());
 }
 
 /**
- * Posts a new measurement corresponding to the trial
- */
-void postMeasurement(String trial, aJsonObject* json) {
-  post("/api/measurement/" + trial, json);
+   Posts a new measurement corresponding to the trial
+*/
+int postMeasurement(String trial, aJsonObject* json) {
+  return post("/api/measurement/" + trial, json);
 }
 
 /**
- * Takes a measurement from the sensor
- * TODO
- */
-String takeMeasurement(String experiment) {
+   Takes a measurement from the sensor
+   TODO -- set up to talk to sensor
+*/
+aJsonObject* takeMeasurement() {
   // get all sensor data, and construct the POST string
-  aJsonObject* meas = constructMeasurement("1", "1", "2", "3", "5", "8");
-  postMeasurement(experiment, meas);
-  return "";
+  return constructMeasurement("1", "1", "2", "3", "5", "8");
+  //  postMeasurement(experiment, meas);
+  //  return "";
 }
 
 /**
- * Collects the requested number of measurements
- */
+   Collects the requested number of measurements
+*/
 void collectData(String experiment, int numExperiments) {
+  aJsonObject* buf[numExperiments];
+  int bufTOS = -1;
   for (int i = 0; i < numExperiments; ++i) {
-    takeMeasurement(experiment);
+    aJsonObject* meas = takeMeasurement();
+    buf[++bufTOS] = meas;
+    while (bufTOS > 0) {
+      int httpCode = postMeasurement(experiment, buf[bufTOS]);
+      if (httpCode != 200) {
+        // we can't post now, wait till later
+        break;
+      } else {
+        // pop off the stack
+        buf[bufTOS--] = nullptr;
+      }
+    }
   }
+  while (bufTOS > 0) {
+    int httpCode = postMeasurement(experiment, buf[bufTOS]);
+    if (httpCode != 200) {
+      // keep trying to post this measurement until we find WiFi
+      connectToWiFi();
+    } else {
+      // pop off the stack
+      buf[bufTOS--] = nullptr;
+    }
+  }
+  delete [] buf;
 }
 

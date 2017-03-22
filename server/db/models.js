7d12560570aed599886ae84ab81c153d5bb61b92
Matthew Faw <matthewfaw@gmail.com>
2017-03-21 23:17:43 -0400

    updates server API to allow posting turbidity and electrochemical measurements

--- server/db/models.js
+++ server/db/models.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
-import { colorMeasurementSchema, trialDataSchema } from './schema.js';
+import { colorMeasurementSchema, electrochemicalMeasurementSchema, trialDataSchema } from './schema.js';
 
 export const ColorMeasurement = mongoose.model('ColorMeasurement', colorMeasurementSchema);
+export const ElectrochemicalMeasurement = mongoose.model('ElectrochemicalMeasurement', electrochemicalMeasurementSchema);
 export const TrialData = mongoose.model('TrialData', trialDataSchema);

6f5fa3a604365ab518453613108184c0c77d3832
Matthew Faw <matthewfaw@gmail.com>
2017-03-22 00:27:49 -0400

    refactors Arduino code to remove the repeated code, and to update the post requests to the new API

--- featherWifiCode/DataCollection.ino
+++ featherWifiCode/DataCollection.ino
@@ -1,5 +1,7 @@
 #include <aJSON.h>
 
+#define MAX_ARRAY_SIZE 5000
+
 /**
    Creates the Trial requested
 */
@@ -8,17 +10,17 @@ int createTrial(String trial) {
 }
 
 /**
-   Posts a new measurement corresponding to the trial
+   Posts a new measurement of the requested type corresponding to the trial
 */
-int postMeasurement(String trial, aJsonObject* json) {
-  return post("/api/measurement/" + trial, json);
+int postMeasurement(String trial, String type, aJsonObject* json) {
+  return post("/api/measurement/" + type + "/" + trial, json);
 }
 
 /**
    Takes a measurement from the sensor
    TODO -- set up to talk to sensor
 */
-aJsonObject* takeMeasurement(Adafruit_TCS34725 tcs) {
+aJsonObject* takeColorMeasurement(Adafruit_TCS34725 tcs) {
   // get all sensor data, and construct the POST string
   uint16_t r, g, b, c, colorTemp, lux;
 
@@ -26,43 +28,39 @@ aJsonObject* takeMeasurement(Adafruit_TCS34725 tcs) {
   colorTemp = tcs.calculateColorTemperature(r, g, b);
   lux = tcs.calculateLux(r, g, b);
   return constructMeasurement(String(r), String(g), String(b), String(c), String(colorTemp), String(lux));
-  //  postMeasurement(experiment, meas);
-  //  return "";
+}
+
+void attemptToPostUnsavedMeasurements(String experiment, aJsonObject * (&buf)[MAX_ARRAY_SIZE], int& bufFRONT, int& bufEND, bool waitToConnect) {
+  while (bufFRONT <= bufEND) {
+    int httpCode = postMeasurement(experiment, "color", buf[bufFRONT]);
+    if (httpCode == 200) { // pop off the stack
+      Serial.println("Send data was successful");
+      buf[bufFRONT++] = nullptr;
+    } else {
+      if (waitToConnect) { // keep trying to post this measurement until we find WiFi
+        while (connectToWiFi() == -1) { //wait to connect
+        }
+      } else {  // we can't post now, wait till later
+        break;
+      }
+    }
+  }
 }
 
 /**
    Collects the requested number of measurements
 */
 void collectData(String experiment, int numExperiments, Adafruit_TCS34725 tcs) {
-  aJsonObject* buf[numExperiments];
-  int bufTOS = -1;
-  for (int i = 0; i < numExperiments; ++i) {
-    aJsonObject* meas = takeMeasurement(tcs);
-    buf[++bufTOS] = meas;
-    while (bufTOS >= 0) {
-      int httpCode = postMeasurement(experiment, buf[bufTOS]);
-      if (httpCode != 200) {
-        // we can't post now, wait till later
-        break;
-      } else {
-        // pop off the stack
-        buf[bufTOS--] = nullptr;
-      }
-    }
+  aJsonObject* buf[MAX_ARRAY_SIZE];
+  int bufFRONT = 0;
+  int bufEND = -1;
+  
+  for (int i = 0; i < min(numExperiments, MAX_ARRAY_SIZE); ++i) {
+    aJsonObject* colorMeas = takeColorMeasurement(tcs);
+    buf[++bufEND] = colorMeas;
+    attemptToPostUnsavedMeasurements(experiment, buf, bufFRONT, bufEND, false);
   }
   Serial.println("Waiting to send data");
-  while (bufTOS >= 0) {
-    int httpCode = postMeasurement(experiment, buf[bufTOS]);
-    if (httpCode != 200) {
-      // keep trying to post this measurement until we find WiFi
-      while (connectToWiFi() == -1) {
-        //wait to connect
-      }
-    } else {
-      // pop off the stack
-      Serial.println("Beginning to send data");
-      buf[bufTOS--] = nullptr;
-    }
-  }
+  attemptToPostUnsavedMeasurements(experiment, buf, bufFRONT, bufEND, true);
 }
 
